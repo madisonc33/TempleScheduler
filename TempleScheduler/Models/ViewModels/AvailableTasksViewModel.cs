c7c08f03490417f6e06523f88258d4e1d389ab1e
amaisey <71740725+amaisey@users.noreply.github.com>
2021-03-15 20:48:44 -0600

    created viewmodels

---
+++ TempleScheduler/Models/ViewModels/AvailableTasksViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TempleScheduler.Models.ViewModels
+{
+    public class AvailableTasksViewModel
+    {
+        public IEnumerable<TimeSlots> AreAvailable { get; set; }
+
+    }
+}
