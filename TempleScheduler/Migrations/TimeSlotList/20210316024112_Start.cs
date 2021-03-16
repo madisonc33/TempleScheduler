12df138fb89e2ce22e2a6b6d93e37be7429dad3e
amaisey <71740725+amaisey@users.noreply.github.com>
2021-03-15 20:45:34 -0600

    got time slot table working

---
+++ TempleScheduler/Migrations/TimeSlotList/20210316024112_Start.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TempleScheduler.Migrations.TimeSlotList
+{
+    public partial class Start : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TimeSlots",
+                columns: table => new
+                {
+                    TimeId = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    AvailableTime = table.Column<DateTime>(nullable: false),
+                    IsAvailable = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TimeSlots", x => x.TimeId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TimeSlots");
+        }
+    }
+}
