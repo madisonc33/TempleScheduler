52a895ba28811e60e4f7f52066aa941b8052eabb
amaisey <71740725+amaisey@users.noreply.github.com>
2021-03-15 18:56:13 -0600

    Update Apppointment.cs

--- TempleScheduler/Models/Apppointment.cs
+++ TempleScheduler/Models/Apppointment.cs
@@ -1,10 +1,27 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+
 namespace TempleScheduler.Models
 {
     public class Apppointment
     {
-        public Apppointment()
-        {
-        }
+        [Key]
+        [Required]
+        public int iAppointmentId { get; set; }
+
+        [Required]
+        public DateTime dApptTime { get; set; }
+
+        [Required]
+        public string sName { get; set; }
+
+        [Required]
+        public int iSize { get; set; }
+
+        [Required]
+        public string sEmail { get; set; }
+
+        [Required]
+        public int iPhoneNum { get; set; }
     }
 }
