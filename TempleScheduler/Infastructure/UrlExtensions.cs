1ea4044fcc947b0ce289787ed54307e85f3eedcd
madisonc33 <61567086+madisonc33@users.noreply.github.com>
2021-03-16 20:56:26 -0600

    adding more comments

--- TempleScheduler/Infastructure/UrlExtensions.cs
+++ TempleScheduler/Infastructure/UrlExtensions.cs
@@ -5,6 +5,8 @@ namespace TempleScheduler.Infrastructure
 {
     public static class UrlExtensions
     {
+        //allows us to pass URLs
+        //generates a URL that the browser will be returned to after the cart has been updated
         public static string PathAndQuery(this HttpRequest request) =>
             request.QueryString.HasValue ? $"{request.Path }{request.QueryString}" : request.Path.ToString();
     }
