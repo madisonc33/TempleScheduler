2aa42ac4725a3d9f819cdcddf48042c4e5dee06b
Kinzie Hamilton <61798938+mackinz2@users.noreply.github.com>
2021-03-17 16:36:55 -0600

    a

--- TempleScheduler/Models/TimeSlotListContext.cs
+++ TempleScheduler/Models/TimeSlotListContext.cs
@@ -3,13 +3,16 @@ using Microsoft.EntityFrameworkCore;
 
 namespace TempleScheduler.Models
 {
+    //List inherits from the Database, used to query and save
     public class TimeSlotListContext : DbContext
     {
+
         public TimeSlotListContext (DbContextOptions<TimeSlotListContext> options) : base(options)
         {
 
         }
 
+        //Set the database to pull Timeslot info
         public DbSet<TimeSlots> TimeSlots { get; set; }
     }
 }
--- TempleScheduler/Models/TimeSlots.cs
+++ TempleScheduler/Models/TimeSlots.cs
@@ -1,18 +1,19 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
 
+//Creates a Model for the TimeSlots Available
 namespace TempleScheduler.Models
 {
     public class TimeSlots
     {
-        [Key]
+        [Key] //Unique Key
         [Required]
         public int TimeId { get; set; }
 
         [Required]
         public DateTime AvailableTime { get; set; }
 
-        [Required]
+        [Required] //If True, will be displayed on the Signup.cshtml page. Change to false through page model after selected
         public bool IsAvailable { get; set; }
     }
 }
